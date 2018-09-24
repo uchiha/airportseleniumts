c96a99042655dc0660ec1279fd118a20dc50e256
Clarencio Tan <clarenciotan@gmail.com>
2018-09-25 10:54:29 +1200

    removed the usingServer option when intializing a driver and Separated method of going to wines and spirits to the Navigator class

--- src/main/common/Navigator.ts
+++ src/main/common/Navigator.ts
@@ -3,9 +3,21 @@ import {BasicHelper} from './BasicHelper';
 import { LocTypes } from '../utils/LocTypes';
 
 export class Navigator{
+    // Shopping cart button
     private static ShopCartLoc: string = "a[class='navigationIconButton navigationIconButtonCart cartButton']";
+
+    //Wines and Spirits
+    private static WinesAndSpiritsLoc : string = "//div[@class='navigationItem']/a[contains(.,'Spirits & Wine')]";
+    private static ViewAllWinesAndSpiritsLoc : string = "//div[@class='navigationDropdownInner']/a[contains(.,'View all Spirits & Wine')]";
 
     public static GoToCart(driver : WebDriver, className: string){
         BasicHelper.click(driver, this.ShopCartLoc, LocTypes.css, className);
     }
+
+    public static GoToWinesAndSpirits(driver: WebDriver, className: string){
+        BasicHelper.click(driver, this.WinesAndSpiritsLoc, LocTypes.xpath, className);
+        BasicHelper.click(driver, this.ViewAllWinesAndSpiritsLoc, LocTypes.xpath, className);
+    }
+
+    
 }
--- src/main/page_objects/ShoppingHomePage.ts
+++ src/main/page_objects/ShoppingHomePage.ts
@@ -1,20 +1,18 @@
 import {BasePage} from '../common/BasePage'
 import {WebDriver } from 'selenium-webdriver';
 import { LocTypes } from '../utils/LocTypes';
+import {Navigator} from '../common/Navigator';
 
 export class ShoppingHomePage extends BasePage{
     
     private driver : WebDriver;
-    private winesAndSpiritsLoc : string = "//div[@class='navigationItem']/a[contains(.,'Spirits & Wine')]";
-    private viewAllWinesAndSpiritsLoc : string = "//div[@class='navigationDropdownInner']/a[contains(.,'View all Spirits & Wine')]";
-
+    
     constructor(driver: WebDriver){
         super(driver);
         this.driver = driver;
     }
 
     public async openWinesAndSpirits(){
-        await this.click(this.winesAndSpiritsLoc, LocTypes.xpath, ShoppingHomePage.name);
-        await this.click(this.viewAllWinesAndSpiritsLoc, LocTypes.xpath, ShoppingHomePage.name);
+       await Navigator.GoToWinesAndSpirits(this.driver, ShoppingHomePage.name);
     }
 }
